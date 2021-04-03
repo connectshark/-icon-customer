8fc234967b86c7de5014a352481bc25162af4381
bobhus394 <bobhus394@gmail.com>
2021-04-03 23:00:07 +0800

    新增重置狀態功能

--- src/components/mainIcon.vue
+++ src/components/mainIcon.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="customer">
+  <div class="customer" :class="{ on: routeName === 'home' }">
     <router-link to="/addIcon" class="btn" :style="{ backgroundColor: iconColor }">
       <span class="material-icons icon">{{iconName}}</span>
     </router-link>
-    <transition name="slide-fade">
-      <div class="user-input" v-if="routeName === 'addIcon'">
-        <input type="text" v-model.trim="iconTitle" class="input" @keydown.enter="addNewIcon">
-      </div>
-    </transition>
+    <div class="user-input" v-if="routeName === 'addIcon'">
+      <input type="text" v-model.trim="iconTitle" class="input" @keydown.enter="addNewIcon">
+    </div>
+    <div class="done-btn" @click="addNewIcon" v-if="iconTitle.length > 0">
+      <span class="material-icons icon">done</span>
+    </div>
   </div>
 </template>
 
@@ -30,7 +31,11 @@ export default {
     const addNewIcon = () => {
       if (iconTitle.value === '') return
       store.commit('addNewIcon', iconTitle.value)
+      store.commit('resetBtn')
+      iconTitle.value = ''
+      router.push('/')
     }
+    
     return {
       iconName,
       iconColor,
@@ -45,13 +50,16 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/scss/transitions.scss';
 .customer{
-  padding: 20px 0;
+  padding: 40px 0;
+  text-align: left;
   .btn{
     display: inline-block;
     vertical-align: middle;
     width: 50px;
     height: 50px;
     border-radius: 25px;
+    text-align: center;
+    margin: 0 50px;
     .icon{
       line-height: 50px;
       font-size: 40px;
@@ -62,16 +70,29 @@ export default {
     display: inline-block;
     vertical-align: middle;
     width: 40%;
-    margin: 0 0 0 50px;
     .input{
       width: 100%;
       border: none;
       padding: 10px 20px;
       box-sizing: border-box;
-      border-radius: 5px;
+      border-radius: 10px;
       min-width: 0;
     }
   }
+  .done-btn{
+    width: 30px;
+    height: 30px;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0 15px;
+    .icon{
+      font-size: 30px;
+      line-height: 1;
+    }
+  }
+}
+.on{
+  text-align: center;
 }
 
 </style>
